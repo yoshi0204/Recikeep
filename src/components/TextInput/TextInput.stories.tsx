a22d329e86b56078d8f3046e0b92beaf2ecce423
末永　陽士 <suenagayoshi0@gmail.com>
2024-02-24 22:52:31 +0900

    moved all files into the recikeep directory

---
+++ src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TextInput from './TextInput';
+
+const meta = {
+  title: 'TextInput',
+  component: TextInput,
+  tags: ['autodocs'],
+} satisfies Meta<typeof TextInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
