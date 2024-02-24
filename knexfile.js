201ea249553531eac75b462976a30970eabd176b
末永　陽士 <suenagayoshi0@gmail.com>
2024-02-24 22:27:22 +0900

    Initial commit with new files and configurations

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+// ESモジュール形式でのKnex設定
+export default {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/dev.sqlite3'
+    },
+    useNullAsDefault: true // SQLiteのための追加設定
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
